6157efccad69d2a3c0341c6af8acca9236a0ba2d
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-31 05:36:37 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,15 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
- 
+ def up
+    change_table :students do |t|
+      t.change :birthdate, :string
+    end
+  end
+
+  def down
+    change_table :students do |t|
+      t.change :birthdate, :string
+    end
+  end
   
 end
